5c640fbfae3400a232ba59b27a0a91ff2b638980
gejun123456 <gejunxxx@163.com>
2015-02-08 15:18:32 +0800

    add basic config

---
+++ src/main/java/com/servlet/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.servlet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by bruce on 2/8/2015.
+ */
+@Controller
+@RequestMapping("/hello")
+public class HelloWorldController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello(ModelMap model) {
+        model.addAttribute("message", "Spring 5 MVC Hello World");
+        return "helloworld";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/helloworld.jsp
@@ -0,0 +1,12 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2/8/2015
+  Time: 14:45
+  To change this template use File | Settings | File Templates.
+--%>
+<html>
+<body>
+<h1>Message : ${message}</h1>
+</body>
+</html>
